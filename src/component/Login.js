4d13ebd54b5b423ee996f6c406b86b07d7636bfa
sonuxkushwaha98 <sonuxkumar98@gmail.com>
2024-12-01 10:59:01 +0530

    updates

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,42 +1,54 @@
-import React, {  useState } from "react";
-import{useNavigate,Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate, Link } from 'react-router-dom'
+import { IoMdEye, IoMdEyeOff } from "react-icons/io"
 
-let Login=()=>{
-    const [email,setemail]=useState('')
-    const [password,setpassword]=useState('')
+let Login = () => {
+    const [email, setemail] = useState('')
+    const [password, setpassword] = useState('')
+    const [pending, setpending] = useState(false)
+    const [visible, setvisible] = useState(true)
     const navigate = useNavigate();
-   
-    const handelLogin= async()=>{
-        console.log(email,password);
+
+    const handelLogin = async () => {
+        console.log(email, password);
         let result = await fetch('https://portfolio-backend-6of6.onrender.com/login', {
             method: "post",
-            body: JSON.stringify({  email, password }),
+            body: JSON.stringify({ email, password }),
             headers: {
                 'Content-Type': 'application/json'
             },
         })
         result = await result.json()
-        console.log ( result)
-        if(result.name){
-            localStorage.setItem('user',JSON.stringify(result))
-          navigate('/')
-        }else{
+        console.log(result)
+        setpending(true)
+        if (result.name) {
+            localStorage.setItem('user', JSON.stringify(result))
+            navigate('/')
+            setpending(false)
+            alert("loging in please wait")
+        } else {
             alert('Please enter correct details')
         }
     }
-    return(
+    return (
         <>
-        <div className="login">
-        <div className="logincontainer">
-            <input className="inputbox" type="email" placeholder="Enter Email" required 
-            onChange={(e)=>{setemail(e.target.value)}} value={email} />
-            <input className="inputbox" type="password" placeholder="Enter Password" required 
-            onChange={(e)=>{setpassword(e.target.value)}} value={password}/>
-            <button className="inputBtn"  onClick={handelLogin}>Login</button>
-            <p>New user then <Link to='/signup' >Sign Up</Link></p>
-        </div>
-        </div>
+            <div className="login">
+                <div className="logincontainer">
+                    <input className="inputbox" type="email" placeholder="Enter Email" required
+                        onChange={(e) => { setemail(e.target.value) }} value={email} />
+                    <div className="password-input">
+                        <input className="passwordbox" type={visible?"password":"text"} required placeholder="Enter your password" value={password}
+                            onChange={(e) => { setpassword(e.target.value) }} />
+                        <div className="eyebtn" onClick={() => { setvisible(!visible) }}>
+                            {visible ? <IoMdEye /> : <IoMdEyeOff />}
+                        </div>
+                    </div>
+                    <button className="inputBtn"  onClick={handelLogin}>{pending ? "Loading..." : "Log in"}</button>
+                    <p>New user then <Link to='/signup' >Sign Up</Link></p>
+                </div>
+            </div>
         </>
-    )}
+    )
+}
 
-    export default Login;
+export default Login;
